dd304e0974e4908c22178eb94ab7167f1eac22b9
Chris Brind <brindy@duckduckgo.com>
2019-08-02 16:19:37 +0100

    Initial Commit

---
+++ MapReveal/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MapReveal
+//
+//  Created by Chris Brind on 02/08/2019.
+//  Copyright © 2019 Chris Brind. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
